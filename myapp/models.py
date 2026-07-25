240115f0fc65e7128e12cd151070fa90a82ad227
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-07-25 22:38:43 +0545

    Made flag triggred

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Case(models.Model):
     case_id = models.IntegerField(primary_key=True, db_column='case_id')
     case_number = models.CharField(max_length=20, unique=True)
@@ -16,6 +17,8 @@ class Case(models.Model):
 
     def __str__(self):
         return self.title
+
+
 class Suspect(models.Model):
     suspect_id = models.IntegerField(primary_key=True, db_column='suspect_id')
     case = models.ForeignKey(Case, related_name="suspects", on_delete=models.CASCADE, db_column='case_id')
@@ -23,8 +26,8 @@ class Suspect(models.Model):
     occupation = models.TextField(null=True, blank=True)
     motive = models.TextField(null=True, blank=True)
     alibi = models.TextField(null=True, blank=True)
-    alibi_status = models.CharField(max_length=50, null=True, blank=True)   
-    suspect_status = models.CharField(max_length=50, null=True, blank=True)   
+    alibi_status = models.CharField(max_length=50, null=True, blank=True)
+    suspect_status = models.CharField(max_length=50, null=True, blank=True)
 
     class Meta:
         db_table = 'suspects'
@@ -36,8 +39,8 @@ class Suspect(models.Model):
 class Evidence(models.Model):
     evidence_id = models.IntegerField(primary_key=True, db_column='evidence_id')
     case = models.ForeignKey(Case, related_name="evidence_items", on_delete=models.CASCADE, db_column='case_id')
-    item_name = models.CharField(max_length=100, db_column='item_name')          
-    evidence_type = models.CharField(max_length=50, db_column='evidence_type')         
+    item_name = models.CharField(max_length=100, db_column='item_name')
+    evidence_type = models.CharField(max_length=50, db_column='evidence_type')
     found_at = models.CharField(max_length=100, db_column='found_at')
     details = models.TextField(null=True, blank=True, db_column='details')
 
@@ -46,3 +49,61 @@ class Evidence(models.Model):
 
     def __str__(self):
         return self.item_name
+
+
+class SolutionSuspect(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    case = models.ForeignKey(Case, on_delete=models.CASCADE, db_column='case_id')
+    suspect = models.ForeignKey(Suspect, on_delete=models.CASCADE, db_column='suspect_id')
+    is_guilty = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'solution_suspects'
+        unique_together = ('case', 'suspect')
+
+
+class SolutionEvidence(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    case = models.ForeignKey(Case, on_delete=models.CASCADE, db_column='case_id')
+    evidence = models.ForeignKey(Evidence, on_delete=models.CASCADE, db_column='evidence_id')
+    is_key_evidence = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'solution_evidence'
+        unique_together = ('case', 'evidence')
+
+
+class CaseSubmission(models.Model):
+    submission_id = models.BigAutoField(primary_key=True)
+    case = models.ForeignKey(Case, related_name='submissions', on_delete=models.CASCADE, db_column='case_id')
+    narrative = models.TextField(null=True, blank=True)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    reviewed = models.BooleanField(default=False)
+    is_correct = models.BooleanField(null=True)
+    reviewer_notes = models.TextField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'case_submissions'
+        ordering = ['-submitted_at']
+
+
+class SubmissionSuspect(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    submission = models.ForeignKey(CaseSubmission, on_delete=models.CASCADE, db_column='submission_id')
+    suspect = models.ForeignKey(Suspect, on_delete=models.CASCADE, db_column='suspect_id')
+    is_accused = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'submission_suspects'
+        unique_together = ('submission', 'suspect')
+
+
+class SubmissionEvidence(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    submission = models.ForeignKey(CaseSubmission, on_delete=models.CASCADE, db_column='submission_id')
+    evidence = models.ForeignKey(Evidence, on_delete=models.CASCADE, db_column='evidence_id')
+    is_selected = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'submission_evidence'
+        unique_together = ('submission', 'evidence')
